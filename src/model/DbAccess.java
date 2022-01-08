2180f909f6895d0df05f5d2ebceec2fda9742b92
JetSpeedFreak <crazygameronline555@gmail.com>
2022-01-08 23:41:43 +0530

    Raw Database layer added

---
+++ src/model/DbAccess.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.*;
+
+public class DbAccess {
+    public static boolean init(){
+        return DbInterface.initialize();
+    }
+
+    // public static ArrayList<ArrayList<Object>> search_houses(){
+
+    // }
+}
---
+++ src/model/DbInterface.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.sql.*;
+
+class DbInterface {
+    private static Connection con;
+
+    private static void commit(){
+        try{
+            Statement smt = con.createStatement();
+            smt.execute("Commit");
+        }catch(SQLException e) {System.out.println("Exception: "+e);}
+    }
+
+    public static boolean initialize(){
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","system");
+            return true;
+        }catch(Exception e){
+            System.out.println("Exeption occured during db init: "+e);
+        }
+        return false;
+    }   
+
+    public static int update_row(String query){
+        try{
+            Statement stmt = con.createStatement();
+            int result = stmt.executeUpdate(query);
+            commit();
+            return result;
+        }catch(Exception e){
+            System.out.println("Exception occured in update_query: "+e);
+        }
+        return -1;
+    }
+    
+    public static int insert_row(String query){
+        try{
+            Statement stmt = con.createStatement();
+            int result = stmt.executeUpdate(query);
+            commit();
+            return result;
+        }catch(Exception e){
+            System.out.println("Exception occured during insert operation: "+e);
+        }
+        return -1;
+    }
+}
